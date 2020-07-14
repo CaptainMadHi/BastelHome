59bff4b5eb7f867677379efe908a7cd2edd08711
oskar.zeino <oskar.zeino@tu-dortmund.de>
2020-07-15 00:51:00 +0200

    FInished main page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,45 +1,81 @@
 <template>
   <div id="app">
-    <b-container>
-      <Device device_name="Mein Licht" device_type="mock_device" :commands="commands" />
+    <b-container v-if="devices && deviceTypes">
+      <h1 class="page-header">BastelHome</h1>
+      <Device
+        v-for="(device, deviceHash) in devices"
+        :key="deviceHash"
+        :deviceName="device.device_name"
+        :deviceType="device.device_type"
+        :deviceHash="deviceHash"
+        :commands="filterCommands(deviceTypes[device.device_type])"
+      />
     </b-container>
   </div>
 </template>
 
 <script>
+import { apiGetDevices, apiGetDeviceTypes } from "./utils";
 import Device from "./components/Device";
-import Command from "./components/Command";
 
 export default {
   name: "App",
   components: {
-    //Command,
     Device
   },
   data: function() {
     return {
-      device: {
-        get: {},
-        change_status: {
-          new_status: "boolean"
-        },
-        change_status2: {
-          new_status: "boolean"
-        },
-        invert_status: {}
+      devices: {
+        "test-hash": {
+          device_name: "Test Device",
+          device_type: "mock_device"
+        }
+      },
+      deviceTypes: {
+        mock_device: {
+          get: {},
+          change_status: {
+            new_status: "boolean"
+          },
+          invert_status: {}
+        }
       }
     };
   },
-  computed: {
-    commands() {
-      let { get, ...filteredCommands } = this.device;
-      return filteredCommands;
+  created() {
+    this.getDeviceTypes();
+    this.getDevices();
+  },
+  methods: {
+    filterCommands(commands) {
+      const commandsClone = JSON.parse(JSON.stringify(commands));
+      delete commandsClone.get;
+      return commandsClone;
+    },
+    async getDeviceTypes() {
+      try {
+        this.deviceTypes = await apiGetDeviceTypes();
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getDevices() {
+      try {
+        this.deviceTypes = await apiGetDevices();
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 };
 </script>
 
 <style>
+h1 {
+  text-align: center;
+  margin-bottom: 2.5rem!important;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
