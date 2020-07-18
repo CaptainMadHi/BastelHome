5d60cf45c80b1fbcfd63c814f87fa3b88cddea10
mahdi.biandooh <mahdi.biandooh@yahoo.de>
2020-07-19 01:40:31 +0200

    added camera device .py

---
+++ surveillance_camera.py
@@ -0,0 +1,64 @@
+import picamera
+import time
+#import socket
+#import struct
+import io
+import socketserver
+from threading import Condition
+from http import server
+
+
+camera = picamera.PiCamera()
+camera.resolution = (1080,720)
+camera.framerate = 30
+camera.exposure_mode = 'night'
+t = time.strftime("%d.%m.%Y-%H%M%S")
+
+
+#---HTML---#
+PAGE="""\
+<html>
+<head>
+<title>Raspberry Pi - Surveillance Camera</title>
+</head>
+<body>
+<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
+</body>
+</html>
+"""
+#----Cam Stuff from https://randomnerdtutorials.com/video-streaming-with-raspberry-pi-camera/----#
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
