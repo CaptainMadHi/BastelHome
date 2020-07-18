291c634a6dfeb2923d4623cb4a4a7b480aa9bcee
oskar.zeino <oskar.zeino@tu-dortmund.de>
2020-07-18 16:09:57 +0200

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
-# setup venv and install packages
+activate() {
+  . "./venv/bin/activate"
+}
 
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+python3 -m venv venv            # setup venv
+activate                        # activate venv
+pip install -r requirements.txt # install python packages into venv
 
+curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n #install node & npm
+bash n lts
+rm n
+
+cd "./frontend"                 
+npm install                     # install JavaScript packages
+npm run build                   # build the website
+cd ".."
 # setup scripts go here ?
 
 # maybe already start the connected component ? 
